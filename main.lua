69b980ff10fdc943cd47138eb2836d88387b0e00
Desmy MJ <desmy.michaelis-joseph@ocsbstudent.ca>
2018-03-01 10:46:25 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,10 @@
+-- Title: MovingObjects
+-- Name: Desmy Michaelis-Joseph
+-- Course: ICS2O/3C
+-- This program displays a beetleship moving
+--across the screen as well as another
+--object moving in a different direction.
+
+print ("***Testing beetleship!")
+
+--Hide 
